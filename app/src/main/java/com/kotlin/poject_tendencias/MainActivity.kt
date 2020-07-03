1b9a560223900891f78ff76ea5f231bfc7a2f8a4
iabotprueba <63275364+iabotprueba@users.noreply.github.com>
2020-07-02 21:51:17 -0500

    commit_2
    actualizado suma simple

--- app/src/main/java/com/kotlin/poject_tendencias/MainActivity.kt
+++ app/src/main/java/com/kotlin/poject_tendencias/MainActivity.kt
@@ -2,6 +2,8 @@ package com.kotlin.poject_tendencias
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.text.set
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Integer.parseInt
@@ -17,15 +19,22 @@ class MainActivity : AppCompatActivity() {
     private fun setup(){
 
         button.setOnClickListener {
-            val numero_1 = parseInt(txtNum1.text.toString())
-            val numero_2 = parseInt(txtNum2.text.toString())
-            val resultado = numero_1 + numero_2
+            showAlert()
 
         }
 
     }
 
     private fun showAlert(){
+        val numero_1 = parseInt(txtNum1.text.toString())
+        val numero_2 = parseInt(txtNum2.text.toString())
+        val resultado = numero_1 + numero_2
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Resultado")
+        builder.setMessage("El resultado es: $resultado")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
 
     }
 }
